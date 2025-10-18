602a5150b5c05c8029f9e5fdccfacc452688f7ab
Petar Popovic <petarpnp@gmail.com>
2025-10-19 00:46:53 +0200

    Update launch mini app screen

--- app/components/launchMiniAppScreen.tsx
+++ app/components/launchMiniAppScreen.tsx
@@ -6,15 +6,15 @@ const LaunchMiniAppScreen = () => {
   return (
     <div className="h-screen flex flex-col items-center justify-center">
       <Image className="rounded-sm" src="/miniapp-logo.jpg" alt="Mini App Logo" width={60} height={60} priority />
-      <p className="mt-10 mb-5 text-sm">Please open Blink Finance Mini App to continue</p>
-      <Button className="mt-5" asChild>
+      <p className="mt-10 mb-5 text-sm text-center max-w-[300px]">Please go to Blink Finance Mini App to continue using the app</p>
+      <Button className="mt-5 w-[250px]" asChild>
         <a href={config.baseAppUrl}>
-          Launch Mini App in Base App
+          Open in <Image src="/Base_basemark_black.svg" alt="Base Logo" width={40} height={40} priority /> app
         </a>
       </Button>
-      <Button className="mt-5" asChild>
+      <Button className="mt-5 w-[250px]" asChild>
         <a href={config.farcasterAppUrl}>
-          Launch Mini App in Farcaster
+          Open in <Image src="/farcaster-logo.svg" alt="Farcaster Logo" width={72} height={72} priority />
         </a>
       </Button>
     </div>
