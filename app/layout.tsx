96c2f54ad6cf06634f33daa686063e73ca80038e
Petar Popovic <petarpnp@gmail.com>
2025-10-19 10:20:44 +0200

    Change default theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
         <body className={`${inter.variable} ${sourceCodePro.variable}`}>
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
           >
