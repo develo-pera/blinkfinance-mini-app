da75638a365710714ffed8f7a4777f0d31c87db8
Petar Popovic <petarpnp@gmail.com>
2025-10-19 13:22:01 +0200

    Update app and minikit configs

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  onlyMiniApp: false,
+  onlyMiniApp: true,
   baseAppUrl: process.env.NEXT_PUBLIC_BASE_APP_URL || 'cbwallet://miniapp?url=https://blinkfinance-mini-app.netlify.app',
   farcasterAppUrl: process.env.NEXT_PUBLIC_FARCASTER_APP_URL || 'https://farcaster.xyz/miniapps/8z40gHY9unFY/blink-finance',
 } as const;
