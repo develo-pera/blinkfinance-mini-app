49c097af5c087eee7100e7205868bfd26d6556bb
Petar Popovic <petarpnp@gmail.com>
2025-10-21 23:55:54 +0200

    Add intaglio bg to company card

--- app/components/pages/profile.tsx
+++ app/components/pages/profile.tsx
@@ -25,7 +25,7 @@ const ProfilePage = ({ user, address }: { user?: Context.UserContext, address?: 
 
   return (
     <div className="px-4 flex flex-col flex-1">
-      <div className="mt-10 px-4">
+      <div className="mt-5 px-4">
         {/* TODO */}
         <h1 className="text-2xl font-bold">{user?.displayName || "Developera"}</h1>
         <p>{displayDomain || truncateAddress(address as Address)}</p>
@@ -35,16 +35,19 @@ const ProfilePage = ({ user, address }: { user?: Context.UserContext, address?: 
 
       <div className="my-10 p-4 bg-[var(--bf-card-background)] rounded-2xl">
         <h2 className="text-3xl">Your Company</h2>
-        <div className="mt-5 bg-[var(--bf-light-green)] dark:bg-[var(--bf-dark-purple)] rounded-xl p-4">
-          <div className="flex items-start justify-between gap-5">
-            <p className="font-bold">Blockops doo</p>
-            <Image src="https://joobpool-production.s3.amazonaws.com/ETH%20Belgrade/eth-bdg.jpg" alt="company logo" width={50} height={50} className="rounded-sm" />
+        <div className="mt-5 bg-[var(--bf-light-green)] dark:bg-[var(--bf-dark-purple)] rounded-xl p-4 relative overflow-hidden">
+          <div className="relative z-1">
+            <div className="flex items-start justify-between gap-5">
+              <p className="font-bold">Blockops doo</p>
+              <Image src="https://joobpool-production.s3.amazonaws.com/ETH%20Belgrade/eth-bdg.jpg" alt="company logo" width={50} height={50} className="rounded-sm" />
+            </div>
+            <div className="mt-15 text-sm">
+              <p>VAT: <span className="opacity-60">1234567890</span></p>
+              <p>Reg. No: <span className="opacity-60">1234567890</span></p>
+              <p>Address: <span className="opacity-60">123 Main St, Anytown, USA</span></p>
+            </div>
           </div>
-          <div className="mt-15 text-sm">
-            <p>VAT: <span className="opacity-60">1234567890</span></p>
-            <p>Reg. No: <span className="opacity-60">1234567890</span></p>
-            <p>Address: <span className="opacity-60">123 Main St, Anytown, USA</span></p>
-          </div>
+          <div className="absolute bg-[url('/intaglio-vector.min.svg')] bottom-0 top-0 right-0 left-0 opacity-20" />
         </div>
       </div>
 
