6e495680a9066916dec25c63c022e34157debf60
Petar Popovic <petarpnp@gmail.com>
2025-10-21 13:25:16 +0200

    Design change

--- app/components/common/navigation-bottom-bar.tsx
+++ app/components/common/navigation-bottom-bar.tsx
@@ -2,18 +2,21 @@ import { cn } from "@/lib/utils";
 import { Context } from "@farcaster/miniapp-sdk";
 import { Address } from "viem";
 import UserAvatar from "./user-avatar";
-import { ReceiptText, SquarePlus } from "lucide-react";
+import { ReceiptText, SquarePlus, Wallet } from "lucide-react";
 
 const NavigationBottomBar = ({ className, user, address }: { className?: string, user?: Context.UserContext, address?: Address }) => {
   return (
     <div className={cn("px-2 py-4 fixed bottom-0 left-0 right-0 z-50", className)}>
       <div className="flex items-center justify-between gap-10 p-4 py-3 dark:bg-[#131313] bg-[#f5f5f5] rounded-sm">
         <div className="flex items-center justify-center">
-          <ReceiptText className="w-8 h-8 text-foreground stroke-[1.5px] opacity-70" />
+          <ReceiptText className="w-6 h-6 text-foreground stroke-[1.5px] opacity-70" />
+        </div>
+        <div className="flex items-center justify-center">
+          <Wallet className="w-6 h-6 text-foreground stroke-[1.5px] opacity-70" />
         </div>
 
         <div className="flex items-center justify-center">
-          <SquarePlus className="w-8 h-8 text-foreground stroke-[1.5px] opacity-70" />
+          <SquarePlus className="w-6 h-6 text-foreground stroke-[1.5px] opacity-70" />
         </div>
         <div className="flex items-center justify-center">
           <UserAvatar onClick={() => { console.log("Click Avatar") }} user={user} address={address} />
